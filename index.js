e3435d1536b786542876feee9dbf8de5523a3fef
AdrianDEChan <31407574+AdrianDEChan@users.noreply.github.com>
2022-03-26 16:22:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+  function drum()
+  {
+  open("https://adriandechan.github.io/drum/");
+  }
+  function github()
+  {
+    open("https://github.com/AdrianDEChan/");
+  }
+  function personal_website()
+  {
+    open("https://adriandechan.github.io/PersonalSite/");
+  }
+  function dogmatch()
+  {
+    open("https://adriandechan.github.io/DogMatch/");
+  }
